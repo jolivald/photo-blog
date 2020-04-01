10de4fb66fc8c6cb8b65cba213eeff7b31f9ccbd
JJonathan <jolivald.jonathan@gmail.com>
2020-04-01 19:01:01 +0200

    add login & register modal to navbar

--- index.php
+++ index.php
@@ -102,29 +102,138 @@ else {
 
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">A World of Faces</a>
+    <a class="navbar-brand" href="index.php">A World of Faces</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
         <div class="collapse navbar-collapse" id="navbarColor01">
             <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">Accueil <span class="sr-only">(actuel)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Order</a>
+                <a class="nav-link" href="#">Commandes</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">About me</a>
+                <a class="nav-link" href="#">A propos de moi</a>
             </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-            <a href="views/register.html" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Register</a>
-            <a href="views/login.html" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Log In</a>
+                <!-- TODO changer boutons en fonction de la session -->
+                
+<?php
+if (isset($_SESSION) && $_SESSION['logged'] === true){
+    echo '<a href="?action=logout&page='.$page.'" class="btn btn-primary btn-lg active">Déconnexion</a>';
+} else {
+    echo '<a href="#" class="btn btn-primary btn-lg active" data-toggle="modal" data-target="#modalLRForm">Connexion</a>';
+}
+?>
             </form>
         </div>
 </nav>
 <!--/ Navbar -->
+
+<!--Modal: Login / Register Form-->
+<div class="modal fade" id="modalLRForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog cascading-modal" role="document">
+      <!--Content-->
+      <div class="modal-content">
+  
+        <!--Modal cascading tabs-->
+        <div class="modal-c-tabs">
+  
+          <!-- Nav tabs -->
+          <ul class="nav nav-tabs md-tabs tabs-2 light-blue darken-3" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active" data-toggle="tab" href="#panel7" role="tab"><i class="fas fa-user mr-1"></i>
+                Se connecter</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="#panel8" role="tab"><i class="fas fa-user-plus mr-1"></i>
+                Créer un compte</a>
+            </li>
+          </ul>
+  
+          <!-- Tab panels -->
+          <div class="tab-content">
+            <!--Panel 7-->
+            <div class="tab-pane fade in show active" id="panel7" role="tabpanel">
+  
+              <!--Body-->
+              <div class="modal-body mb-1">
+                <form method="POST" action="?action=login&page=<?= $page ?>">
+                    <div class="md-form form-sm mb-5">
+                        <i class="fas fa-envelope prefix"></i>
+                        <input type="email" id="modalLRInput10" class="form-control form-control-sm validate">
+                        <label data-error="wrong" data-success="right" for="modalLRInput10">Email</label>
+                    </div>
+    
+                    <div class="md-form form-sm mb-4">
+                        <i class="fas fa-lock prefix"></i>
+                        <input type="password" id="modalLRInput11" class="form-control form-control-sm validate">
+                        <label data-error="wrong" data-success="right" for="modalLRInput11">Mot de passe</label>
+                    </div>
+                    <div class="text-center mt-2">
+                        <button class="btn btn-info">Connexion<i class="fas fa-sign-in ml-1"></i></button>
+                    </div>
+                </form>
+              </div>
+              <!--Footer-->
+              <div class="modal-footer">
+                <div class="options text-center text-md-right mt-1">
+                  <p>Pas membre? <a href="#" class="blue-text">Créer un compte</a></p>
+                  <p><a href="#" class="blue-text">Mot de passe oublié?</a></p>
+                </div>
+                <button type="button" class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Fermer</button>
+              </div>
+  
+            </div>
+            <!--/.Panel 7-->
+  
+            <!--Panel 8-->
+            <div class="tab-pane fade" id="panel8" role="tabpanel">
+  
+              <!--Body-->
+              
+                <div class="modal-body">
+                    <form method="POST" action="?action=register&page=<?= $page ?>">
+                        <div class="md-form form-sm mb-5">
+                            <i class="fas fa-envelope prefix"></i>
+                            <input type="email" id="modalLRInput12" class="form-control form-control-sm validate">
+                            <label data-error="wrong" data-success="right" for="modalLRInput12">Email</label>
+                        </div>
+                        <div class="md-form form-sm mb-5">
+                            <i class="fas fa-lock prefix"></i>
+                            <input type="password" id="modalLRInput13" class="form-control form-control-sm validate">
+                            <label data-error="wrong" data-success="right" for="modalLRInput13">Mot de passe</label>
+                        </div>
+                        <div class="md-form form-sm mb-4">
+                            <i class="fas fa-lock prefix"></i>
+                            <input type="password" id="modalLRInput14" class="form-control form-control-sm validate">
+                            <label data-error="wrong" data-success="right" for="modalLRInput14">Répéter le mot de passe</label>
+                        </div>
+                        <div class="text-center form-sm mt-2">
+                            <button class="btn btn-info">Inscription<i class="fas fa-sign-in ml-1"></i></button>
+                        </div>
+                    </form>
+                </div>
+                <!--Footer-->
+                <div class="modal-footer">
+                    <div class="options text-right">
+                      <p class="pt-1">Déjà un compte? <a href="#" class="blue-text">Se connecter</a></p>
+                    </div>
+                    <button type="button" class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Fermer</button>
+                </div>
+            </div>
+            <!--/.Panel 8-->
+          </div>
+  
+        </div>
+      </div>
+      <!--/.Content-->
+    </div>
+</div>
+<!--Modal: Login / Register Form-->
 
 <!-- Container -->
 <div class="container my-4">
