ac1620c7a7f99867a4ff12abf2157c15f1c45f32
kevinho19 <kevin.b_19@hotmail.fr>
2020-04-02 18:42:25 +0200

    finition

--- index.php
+++ index.php
@@ -124,8 +124,6 @@ else {
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
-    <link rel="stylesheet" href="cards-gallery.css">
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -216,7 +214,7 @@ if (isset($_SESSION['logged'])){
               <!--Footer-->
               <div class="modal-footer">
                 <div class="options text-center text-md-right mt-1">
-                  <p>Pas membre? <a href="#" class="blue-text">Créer un compte</a></p>
+                  <p>Pas membre? <a href="#panel8" class="blue-text">Créer un compte</a></p>
                   <p><a href="#" class="blue-text">Mot de passe oublié?</a></p>
                 </div>
                 <button type="button" class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Fermer</button>
